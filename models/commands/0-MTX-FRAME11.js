aff1f702f3b62be054c57251be2f470b0ef7e69b
uzair-aroob <8uzairrajput@gmail.com>
2025-03-29 21:50:51 +0500

    Update 0-MTX-FRAME11.js

--- models/commands/0-MTX-FRAME11.js
+++ models/commands/0-MTX-FRAME11.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(500, 500), 17000, 540).composite(circleTwo.resize(890, 890), 1220, 1210);
+  batgiam_img.composite(circleOne.resize(500, 500), 17000, 540).composite(circleTwo.resize(890, 890), 1240, 1250);
 
   let raw = await batgiam_img.getBufferAsync("image/jpeg");
 

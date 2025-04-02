70a321c4b12a4bfc3fa04f0612cc11a9f3cb20f8
uzair-aroob <8uzairrajput@gmail.com>
2025-04-03 01:08:58 +0500

    Update pair11.js

--- models/commands/pair11.js
+++ models/commands/pair11.js
@@ -41,7 +41,7 @@ async function makeImage({ one, two }) {
 
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));9
-    pairing_img.composite(circleOne.resize(190, 190), 94, 129).composite(circleTwo.resize(190, 190), 531, 129);
+    pairing_img.composite(circleOne.resize(190, 190), 92, 128).composite(circleTwo.resize(190, 190), 530, 128);
 
     let raw = await pairing_img.getBufferAsync("image/png");
 

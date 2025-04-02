b79a14f03ecb7712ee35a6e5c98e729e694130ae
uzair-aroob <8uzairrajput@gmail.com>
2025-04-02 20:01:54 +0500

    Update 0_LOVE2.js

--- models/commands/0_LOVE2.js
+++ models/commands/0_LOVE2.js
@@ -45,7 +45,7 @@ async function makeImage({ one, two }) {
 
   let circleOne = await jimp.read(await circle(avatarOne));
   let circleTwo = await jimp.read(await circle(avatarTwo));
-  batgiam_img.composite(circleOne.resize(73, 73), 15, 26).composite(circleTwo.resize(73, 73), 151, 26);
+  batgiam_img.composite(circleOne.resize(74, 74), 16, 26).composite(circleTwo.resize(73, 73), 150, 26);
 
   let raw = await batgiam_img.getBufferAsync("image/jpeg");
 

08923a09d2732341c87003bdff9fd6aa3f8e7b1b
uzair-aroob <8uzairrajput@gmail.com>
2025-04-04 02:34:47 +0500

    Update setdatabox.js

--- models/commands/setdatabox.js
+++ models/commands/setdatabox.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "setdatabox",
     version: "1.0",
     hasPermssion: 2,
-    credits: "D-Jukie",
+    credits: "uzairrajput",
     description: "Set new data of boxes into data",
     commandCategory: "System",
     usages: "",

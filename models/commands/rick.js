65070d7061d7f482dcb682f511d49a4d31596090
uzair-aroob <8uzairrajput@gmail.com>
2025-04-03 23:27:37 +0500

    Update rick.js

--- models/commands/rick.js
+++ models/commands/rick.js
@@ -3,7 +3,7 @@ module.exports.config = {
 	name: "rick",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Joshua Guerrero", 
+	credits: "uzairrajput", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -12,10 +12,10 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
-	if(event.body.includes("rickrolled") || event.body.includes("rick") || event.body.includes("RICKROLLED") || event.body.includes("RICK"))  {
+	if(event.body.includes("Matlabi") || event.body.includes("MATLABI") || event.body.includes("matlabi") || event.body.includes("test"))  {
 		var msg = {
-				body: "You got rickrolled",
-				attachment: fs.createReadStream(__dirname + `/noprefix/rickrolled.mp3`)
+				body: "Bat Karwai Hai Magar Saxh Hai.......🥀💫💔",
+				attachment: fs.createReadStream(__dirname + `/uzair/Matlabi.mp4`)
 			}
 			api.sendMessage(msg, threadID, messageID);
   }

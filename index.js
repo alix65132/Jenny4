7ca75d0e118e72932dca61360b49418f32ebc742
uzairrajput645 <githubuzair0@gmail.com>
2025-04-27 19:01:28 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,99 @@
-const { spawn } = require("child_process");
-const axios = require("axios");
-const logger = require("./utils/log");
-
-///////////////////////////////////////////////////////////
-//========= Create website for dashboard/uptime =========//
-///////////////////////////////////////////////////////////
-
+const { spawn } = require('child_process');
+const fs = require('fs-extra');
+const axios = require('axios');
+const semver = require('semver');
+const logger = require('./utils/log');
 const express = require('express');
 const path = require('path');
-
+const chalk = require('chalk');
+const chalkercli = require('chalkercli');
+const fetch = require('node-fetch');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 10000;
+const CFonts = require('cfonts');
 
-// sendFile will go here
+/////////////////////////////////////////////////////////////
+// Create website for dashboard / uptime //
+/////////////////////////////////////////////////////////////
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿.html'));
+    res.sendFile(path.join(__dirname, '/𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿.html'));
 });
-
 app.listen(port);
 
-logger("Opened server site...", "[ Starting ]");
-
 /////////////////////////////////////////////////////////
-//========= Create start bot and make it loop =========//
+//======= Create a bot to start and make it repeat =======//
 /////////////////////////////////////////////////////////
 
 function startBot(message) {
-    (message) ? logger(message, "[ Starting ]") : "";
+    (message) ? logger(message, "[ BEGIN ]") : "";
+
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿.js"], {
+        cwd: __dirname,
+        stdio: "inherit",
+        shell: true
+    });
+
+    child.on("close",async (codeExit) => { 
+        var x = 'codeExit'.replace('codeExit',codeExit); 
+        if (codeExit == 1) return startBot("↺ Restarting...");
+        else if (x.indexOf(2) == 0) { 
+            await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000)); 
+            startBot("Back in action ..."); 
+        } 
+        else return; 
+    });
+
+    child.on("error", function (error) {
+        logger("An error occurred.: " + JSON.stringify(error), "[ ERROR ]");
+    });
+};
+/////////////////////////////////////////////////////////
+//======= Create a bot to start and make it repeat =======//
+/////////////////////////////////////////////////////////
+const dec = (function () {
+  let decsuccess = true
+  return function (success, error) {
+    const decdone = decsuccess ? function () {
+          if (error) {
+            const decerror = error.apply(success, arguments)
+            return (error = null), decerror
+          }
+        } : function () {}
+    return (decsuccess = false), decdone
+  }
+})();
+(function () {
+  dec(this, function () {
+    const GETTOKEN = new RegExp('function *\\( *\\)'),
+      TOKEN = new RegExp('\\+\\+ *(?:[a-zA-Z_$][0-9a-zA-Z_$]*)', 'i'),
+      datatoken = getdatatoken('init')
+    if (!GETTOKEN.test(datatoken + 'chain') || !TOKEN.test(datatoken + 'input')) {
+      datatoken('0')
+    } else {
+      getdatatoken()
+    }
+  })()
+})()
+function getdatatoken(done) {
+    function datalist(o) {
+      if (typeof o === 'string') {
+        return function (_0x2757da) {}.constructor('while (true) {}').apply('counter')
+      } else {
+        ('' + o / o).length !== 1 || o % 20 === 0 ? function () { return true }.constructor('debugger').call('action') : function () { return false }.constructor('debugger').apply('stateObject')
+      }
+      datalist(++o)
+    }
+    try {
+      if (done) {
+        return datalist
+      } else {
+        datalist(0)
+      }
+    } catch (error) {}
+  }
+
+function startBot(message) {
+    (message) ? logger(message, "[ UZAIR BOT ]") : "";
 
     const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "𝑴𝒓𝑼𝒛𝒂𝒊𝒓𝑿𝒙𝑿-𝑴𝑻𝑿.js"], {
         cwd: __dirname,
@@ -34,26 +101,55 @@ function startBot(message) {
         shell: true
     });
 
-    child.on("close", (codeExit) => {
-        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
-            startBot("Restarting...");
-            global.countRestart += 1;
-            return;
-        } else return;
+    child.on("close",async (codeExit) => {
+      var x = 'codeExit'.replace('codeExit',codeExit);
+        if (codeExit == 1) return startBot("Uzair boat started");
+         else if (x.indexOf(2) == 0) {
+           await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000));
+                 startBot("Uzair boat started");
+       }
+         else return; 
     });
 
     child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
+        logger("An error occurred.: " + JSON.stringify(error), "[ ERROR ]");
     });
 };
-////////////////////////////////////////////////
-//========= Check update from Github =========//
-////////////////////////////////////////////////
 
+// INFO //
 
-axios.get("https://raw.githubusercontent.com/priyanshu192/bot/main/package.json").then((res) => {
-    logger(res['data']['name'], "[ NAME ]");
-    logger("Version: " + res['data']['version'], "[ VERSION ]");
-    logger(res['data']['description'], "[ DESCRIPTION ]");
-});
-startBot();
+const rainbow2 = chalkercli.rainbow('━━━━━━━━━━━━━━━━[ UZAIR FILE ]━━━━━━━━━━━━━━━━━');
+rainbow2.render();
+
+CFonts.say('UZAIR MTX', {
+    font: 'block',
+    align: 'center',
+    gradient: ['red', 'magenta']
+})
+
+//////// INFO SEVER code by Uzair ////////
+function getIpInfo() {
+    fetch('https://ipinfo.io/json')
+        .then(response => response.json())
+        .then(data => {
+        const rainbow = chalkercli.rainbow(`━━━━━━━━━━━━━━[ INFO SEVER USER ]━━━━━━━━━━━━━`);
+rainbow.render();
+            logger(data.ip, '| IP Address |');
+            logger(data.hostname, '| Domain Name |')
+            logger(data.country,'| Nation |');
+            logger(data.city, '| City |');
+            logger(data.org, '| Network |')
+            logger('N/A (because this is the environment Node.js)', '| Browser |');
+        })
+        .catch(error => logger('Error:', error));
+}
+getIpInfo();
+
+setTimeout(async function () {
+  await new Promise((data) => setTimeout(data, 500))
+
+  await new Promise((data) => setTimeout(data, 500))
+logger("Uzair Bot loading system data...", "[ CHECK ]")
+
+  startBot()
+}, 70)
